ad647f3f87397b8a697789c03be13eb5e9101229
NZSmartie <nzsmartie@gmail.com>
2017-10-05 22:40:15 +1300

    Improve splach screen for Android

--- src/OICExplorer.Droid/MainActivity.cs
+++ src/OICExplorer.Droid/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace OICExplorer.Droid
 {
-    [Activity(Label = "OIC Explorer", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/AppTheme")]
+    [Activity(Label = "OIC Explorer", Icon = "@drawable/icon", Theme = "@style/AppTheme")]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -14,8 +14,6 @@ namespace OICExplorer.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new OICExplorer.App());
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
         }
     }
 }
---
+++ src/OICExplorer.Droid/SplashActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Content;
+using Android.Support.V7.App;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace OICExplorer.Droid
+{
+    [Activity(Label = "OIC Explorer", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/AppTheme.Splash", NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
+
